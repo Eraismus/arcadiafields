a69dee7fde6a9e35be45e9c5de137092e7db7f02
Eraismus <jorgenivar@protonmail.com>
2025-07-22 20:26:18 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const button = document.getElementById('myButton');
+    button.addEventListener('click', () => {
+        alert('Hello! You clicked the button.');
+    });
+});
